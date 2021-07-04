00f8762ddbfb34f64ab0c43ff996b3ee8197a378
Virgile Guglielmi <virgile.gug@gmail.com>
2021-07-04 15:01:44 +0200

    :sparkles: email_crypt
    email_crypt

--- backend/middleware/crypto.js
+++ backend/middleware/crypto.js
@@ -3,10 +3,6 @@ const algorithm = `${process.env.CRYPTO_ALGORITHM}`
 const secretKey = `${process.env.CRYPTO_KEY}`
 const iv = Buffer.from(`${process.env.CRYPTO_IV}`, 'hex')
 
-//const algorithm = 'aes-256-ctr'
-//const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3'
-//const iv = Buffer.from('834d0b9a674e80448b8a0eeca8ed9d44', 'hex')
-
 const crypto = require('crypto')
 
 const encrypt = (text) => {
